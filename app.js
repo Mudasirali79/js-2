59afd1c21af934f309064b472342f2934392d327
Mudasirali79 <160850781+Mudasirali79@users.noreply.github.com>
2024-02-29 18:44:46 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,77 @@
+// let Array = ["40","chor",123,455,"bhai"];
+// Array.push(999)
+// console.log(Array);
+
+// -------------------------------------------------------------->
+
+// let Array = ["40","chor",123,455,"bhai"];
+// Array.pop()
+// console.log(Array)
+
+// ------------------------------------------------------------------>
+
+// let Array = ["40","chor",123,455,"bhai"];
+// Array.unshift("BAYA")
+// console.log(Array)
+
+// ------------------------------------------------------------------->
+// let Array = ["40","chor",123,455,"bhai"];
+//  Array.shift()
+// console.log(Array)
+
+// ---------------------------------------------------------------------->
+
+// let Array = ["40","chor",123,455,"bhai"];
+//   Array.splice(1,2)
+//   console.log(Array)
+
+// --------------------------------------------------------------------------->
+
+
+// let Array = ["40","chor",123,455,"bhai"];
+//  let arr2 = Array.slice(1,3)
+//   console.log(Array)
+//   console.log(arr2)
+
+// --------------------------------------------------------------------------->
+
+// let arr=["mushtaq","ali",123,true,"bilal"];
+
+// let result = arr.includes("ali")
+
+// console.log(result);
+
+
+// let arr=["mushtaq","ali",123,true,"bilal"];
+
+// let result = arr.indexOf("bilal")
+
+// console.log(result);
+// --------------------------------------------------------------------------->
+
+// let arr=["mushtaq","ali",123,true,"bilal"];
+// let arr1=["mushtaq","ali",123,true,"bilal",126,555];
+
+// let result = arr1.concat(arr)
+
+//  console.log(result);
+
+// --------------------------------------------------------------------------->
+
+//  let num=[9,8,7,6,5,4,3,2,1];
+
+// let result = num.sort()
+
+//  console.log(result);
+
+// --------------------------------------------------------------------------->
+
+// let tablevalue = +prompt ("Enter The Table Value");
+// let tablestartvalue = +prompt ("Enter The Starting Value");
+// let tableendvalue = +prompt ("Enter The End Value");
+
+// for ( let i = tablestartvalue; i <= tableendvalue; i++ ){
+// document.write( tablevalue + " x " + i + " = " + tablevalue*i + "<br>" )
+// }   
+
+// --------------------------------------------------------------------------------->
